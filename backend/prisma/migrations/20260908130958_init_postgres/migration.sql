07b8d3f8e6ced7a8af67637a085abadd02ac824c
Ece Erol <ecee.eroll00@gmail.com>
2026-09-08 16:20:39 +0300

    Migrate database from SQLite to PostgreSQL

--- backend/prisma/migrations/20260908130958_init_postgres/migration.sql
+++ backend/prisma/migrations/20260908130958_init_postgres/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "Book" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "author" TEXT NOT NULL,
     "genres" JSONB NOT NULL,
     "pageCount" INTEGER NOT NULL,
     "publishedYear" INTEGER NOT NULL,
-    "goodreadsRating" REAL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "goodreadsRating" DOUBLE PRECISION,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
 );
